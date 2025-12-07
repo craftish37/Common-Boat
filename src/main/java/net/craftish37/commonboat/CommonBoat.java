2a1892c4a61f362ab37103d8f1faf10ad26186b8
craftish37 <craftish37@gmail.com>
2025-12-07 16:08:57 +0100

    Update CommonBoat.java

--- src/main/java/net/craftish37/commonboat/CommonBoat.java
+++ src/main/java/net/craftish37/commonboat/CommonBoat.java
@@ -3,6 +3,7 @@ package net.craftish37.commonboat;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -45,6 +46,11 @@ public class CommonBoat implements ClientModInitializer {
         Sounds.registerSounds();
         ConfigAccess.get();
         EasterEggFishHighlighter.startUpdater();
+
+        // Register the render event using AFTER_TRANSLUCENT (safest for lines) or LAST
+        WorldRenderEvents.END_MAIN.register(context -> {
+            EasterEggFishHighlighter.onWorldRender(context.matrices());
+        });
         masterToggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.commonboat.toggle",
                 InputUtil.Type.KEYSYM,
